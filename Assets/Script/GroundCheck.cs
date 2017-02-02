ad4cdde0a679c56bf15fe59d5a7aab4b257450f4
Charles Wijaya <charleswijaya@Charless-MacBook-Pro-2.local>
2017-02-02 21:08:17 +0700

    first commit

---
+++ Assets/Script/GroundCheck.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour {
+	private Player player;
+	// Use this for initialization
+	void Start () {
+		player = gameObject.GetComponent<Player> ();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject==player.gameObject) {
+			Debug.Log ("hello");
+		}
+
+	}
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	private Animator anim;
+	private float speed=25f;
+	private float jump=150f;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = gameObject.GetComponent<Rigidbody2D> ();
+		anim = gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void FixedUpdate(){
+		if (Mathf.Abs(Input.GetAxis ("Horizontal"))>0.1f) {
+			anim.SetBool ("walk", true);
+		} else {
+			anim.SetBool ("walk", false);}
+		float h = Input.GetAxis ("Horizontal");
+	
+		if (Input.GetAxis("Horizontal")>0.1f){
+			transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+			rb2d.AddForce ((Vector2.right * speed) * h);
+		}
+
+		if (Input.GetAxis("Horizontal")<-0.1f){
+			transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+			rb2d.velocity= new Vector2(h, 0);
+		}
+
+		if (Input.GetButtonDown("Jump")) {
+			rb2d.AddForce (Vector2.up * jump);
+		}
+		//rb2d.AddForce ((Vector2.right * speed) * h);
+
+	}
+}
+	
+
